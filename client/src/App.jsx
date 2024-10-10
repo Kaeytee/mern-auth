3e50135af32f0805a2daa7e0749ed27d5e78f8b4
kaeytee <austinbediako4@gmail.com>
2024-10-10 23:47:17 +0000

    Refactor App.jsx and add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import About from "./pages/About"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 export default function App() {
   return (
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/sign-in" element={<Signin />} />
-    <Route path="/sign-up" element={<Signup />} />
-    <Route path="/profile" element={<Profile />} />
-  </Routes>
+  {/*header*/}
+  <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   </BrowserRouter>
   )
 }
