dd1bbe85fab29bf12fa4018c82bd32781f897f03
kaeytee <austinbediako4@gmail.com>
2024-10-11 22:47:00 +0000

    completed the signin page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Signin from "./pages/SignIn"
-import Signup from "./pages/SignUp"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/SignIn";
+import Signup from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-  <BrowserRouter>
-  {/*header*/}
-  <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<Signin />} />
-      <Route path="/sign-up" element={<Signup />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      {/*header*/}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        {/*404 page*/}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
